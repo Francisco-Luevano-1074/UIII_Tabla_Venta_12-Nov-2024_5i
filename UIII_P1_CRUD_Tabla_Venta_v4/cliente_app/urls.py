860653c0fd0e8a2f8cdc86bb34bc5e5b606c9e2f
Francisco Luévano <164532299+Francisco-Luevano-1074@users.noreply.github.com>
2024-11-19 13:02:37 -0600

    Add files via upload

---
+++ UIII_P1_CRUD_Tabla_Venta_v4/cliente_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from cliente_app import views
+
+urlpatterns = [
+    path('', views.Inicio_vista, name='inicio'),
+    path('registrarCliente/', views.RegistrarCliente, name='registrar_cliente'),
+    path('editarCliente/', views.editarCliente, name='editar_cliente'),
+    path('seleccionarCliente/<int:codigo>/', views.SeleccionarCliente, name='seleccionar_cliente'),
+    path('borrarCliente/<int:codigo>/', views.BorrarCliente, name='borrar_cliente'),
+]
---
+++ UIII_P1_CRUD_Tabla_Venta_v4/cliente_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .models import Cliente
+
+# Create your views here.
+
+def Inicio_vista(request):
+    losclientes=Cliente.objects.all()
+    return render(request, 'gestionarCliente.html', {'misclientes' : losclientes})
+
+def RegistrarCliente(request):
+    codigo=request.POST['txtcodigo']
+    nombre=request.POST['txtnombre']
+    apellido=request.POST['txtapellido']
+    no_telefono=request.POST['txtno_telefono']
+    email=request.POST['txtemail']
+    orden=request.POST['txtorden']
+    mesa=request.POST['txtmesa']
+
+    guardarclientes=Cliente.objects.create(codigo=codigo, nombre=nombre, apellido=apellido, no_telefono=no_telefono,email=email,orden=orden,total=mesa)
+    return redirect ("/")
+
+
+
+
+def SeleccionarCliente(request, codigo):
+    cliente = Cliente.objects.get(codigo=codigo)
+    return render(request, "editarCliente.html", {"misclientes": cliente})
+
+
+def editarCliente(request):
+    if request.method == "POST":
+        codigo = request.POST['txtcodigo']
+        try:
+            cliente = Cliente.objects.get(codigo=codigo)
+            cliente.nombre = request.POST['txtnombre']
+            cliente.apellido = request.POST['txtapellido']
+            cliente.no_telefono= request.POST['txtno_telefono']
+            cliente.email = request.POST['txtemail']
+            cliente.orden = request.POST['txtorden']
+            cliente.mesa = request.POST['txtmesa']
+            cliente.save()
+        except Cliente.DoesNotExist:
+            # Manejo del error si el cliente no existe
+            pass
+    return redirect("/")
+
+
+
+def BorrarCliente(request,codigo):
+    cliente=Cliente.objects.get(codigo=codigo)
+    cliente.delete()
+    return redirect("/")
